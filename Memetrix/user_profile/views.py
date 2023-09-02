428b3065702879cb6632f3ce394bedc9d4f8b821
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-03 00:04:09 +0500

    Created an app user_profile to display the user his data that can only be updated and deleted by the respective user

--- Memetrix/user_profile/views.py
+++ Memetrix/user_profile/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def profile(request):
+    return HttpResponse("<h1>This is the profile page</h1>")
