6a825cbcc955a280e9d15a5f89f7bf401a3afa23
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-03 05:15:11 +0500

    Created a layout for the feeds section of the website although the layout is static but the user can only access the feed page only if he is authenticated

--- Memetrix/feeds/views.py
+++ Memetrix/feeds/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(redirect_field_name="login_required", login_url="/login/")
 def feeds(request):
-    return HttpResponse("This is feeds")
+    return render(request, "feeds.html", context={"title": "MemeTrix | Feeds"})
