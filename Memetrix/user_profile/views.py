60a38c678512bf5d8c5296b0e98a0eaed6bc0df7
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-03 05:16:28 +0500

    Created a function to show an http response when a user go to the profile url. Will work on the profile page later

--- Memetrix/user_profile/views.py
+++ Memetrix/user_profile/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(redirect_field_name="login_required", login_url="/login/")
 def profile(request):
     return HttpResponse("<h1>This is the profile page</h1>")
