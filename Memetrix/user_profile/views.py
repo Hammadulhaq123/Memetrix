8fa29cb406559cb8ab1a783ec184f72a52844bf7
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-04 00:44:13 +0500

    Updated the interface of the profile and updated few backend functionalities

--- Memetrix/user_profile/views.py
+++ Memetrix/user_profile/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.contrib import messages
 
 # Create your views here.
 
 
 @login_required(redirect_field_name="login_required", login_url="/login/")
-def profile(request):
+def profile(request, id):
     return render(request, "profile.html")
+
+
+def update_name(request, id):
+    pass
+
+
+def update_pass(request, id):
+    pass
+
+
+def update_number(request, id):
+    pass
+
+
+def delete_account(request, id):
+    try:
+        queryset = User.objects.get(id=id)
+        queryset.delete()
+        logout(request)
+        messages.success(request, "Account deleted succesfully!")
+    except:
+        messages.error(request, "Internal Server error")
+    return redirect("/signup/")
